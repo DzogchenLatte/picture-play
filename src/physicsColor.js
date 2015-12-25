d92f4d40e252d0f9584f1d9cca4dfd712ce9bc72
Andrew Decker <ajdecker1022@gmail.com>
2015-12-25 08:13:43 -0600

    More work with pinball bouncing.  Matter JS has a bug where the pinball sticks to the boundary

--- src/physicsColor.js
+++ src/physicsColor.js
@@ -4,36 +4,62 @@ var Engine = Matter.Engine,
     Bodies = Matter.Bodies
     Vector = Matter.Vector;
 
-var engine = Engine.create(document.body);
-
-var circles = [];
 var worldWidth = 800;
 var worldHeight = 600;
+var xCenter = worldWidth / 2;
+var yCenter = worldHeight / 2;
+
+var renderObject = {
+	options: {
+		width: worldWidth,
+		height: worldHeight
+	}
+};
+
+var engine = Engine.create(document.body, {
+	render: renderObject,
+	velocityIterations: 10
+});
+
+engine.world.bounds.min.x = -Infinity;
+engine.world.bounds.min.y = -Infinity;
+engine.world.bounds.max.x = Infinity;
+engine.world.bounds.max.y = Infinity;
+
+var circles = [];
 
 var startRadius = 1;
 var radiusRate = 20;
 var numCircles = 100;
 var spiralRate = 12;
 
-var pinballRadius = 10;
+var pinballRadius = 5;
 
 var velocityVector = {
-	x: 3,
-	y: 0
+	x: 6,
+	y: 6
 };
 
-
-var bottom = Bodies.rectangle(400, 610, 810, 60, {
+var staticParams = {
 	isStatic: true,
 	friction: 0,
  	frictionStatic: 0,
- 	frictionAir: 0
- });
+ 	frictionAir: 0,
+ 	restitution: 1
+};
+
+//for some reason, "top" === Window...not sure how that conflict arose or persisted
+var bottom = Bodies.rectangle(xCenter, worldHeight, worldWidth, 10, staticParams);
+var left = Bodies.rectangle(0, yCenter, 10, worldHeight, staticParams);
+var topBound = Bodies.rectangle(xCenter, 0, worldWidth, 10, staticParams);
+var right = Bodies.rectangle(worldWidth, yCenter, 10, worldHeight, staticParams);
 
 var pinball = Bodies.circle(100, 100, pinballRadius, {
 	friction: 0,
  	frictionStatic: 0,
- 	frictionAir: 0
+ 	frictionAir: 0,
+ 	restitution: 1,
+ 	inertia: Infinity
 });
 
 pinball.render.fillStyle = "#FF0000";
@@ -41,12 +67,21 @@ Body.setVelocity(pinball, velocityVector);
 
 generateSpiral(numCircles, spiralRate, radiusRate);
 
-World.add(engine.world, [bottom]);
+World.add(engine.world, [left, bottom, right, topBound]);
 World.add(engine.world, [pinball]);
 World.add(engine.world, circles);
 
 engine.world.gravity.y = 0;
 Engine.run(engine);
+
+//artificially maintain velocity
+setInterval(function() {
+	pinball.Inertia = Infinity;
+}, 10);
+
+setInterval(function() {
+	console.log(pinball);
+}, 1000);
 
 function generateSpiral(numCircles, spiralRate, radiusRate) {
 	var numRotations = 4;
@@ -58,14 +93,15 @@ function generateSpiral(numCircles, spiralRate, radiusRate) {
 		var y =  spiralRate * t * Math.sin(t);
 		var thisRadius = startRadius + radiusRate * (i / numCircles); 
 
-		x += worldWidth / 2;
-		y += worldHeight / 2;
+		x += xCenter;
+		y += yCenter / 2 + 170;
 
 		circles.push( Bodies.circle(x, y, thisRadius, {
 			isStatic: true,
 			friction: 0,
 		 	frictionStatic: 0,
-		 	frictionAir: 0
+		 	frictionAir: 0,
+		 	restitution: 1
 		 }) );
 	}
 	console.log(circles);
